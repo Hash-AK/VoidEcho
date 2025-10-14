5943bf4971cc5cb616ae6d53e0a5318dee0d64c7
Hash-AK <175879022+Hash-AK@users.noreply.github.com>
2025-10-14 16:14:19 -0400

    first real code commit, added the map for the base's exterior. dont know yet how i will make the unlock door mechanism

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+type Game struct {
+	World    [][]int
+	Player   Player
+	PowerOn  bool
+	KnownMap [][]bool
+}
+type Player struct {
+	X         int
+	Y         int
+	Inventory map[string]*Item
+}
+type Item struct {
+	Name        string
+	Description string
+}
 
+func main() {
+	// 0 mean empty space
+	// 1 means wall
+	// 2 means locked door
+	// 3 means stuf you gotta fidn to unlock the door
+	worldMap := [][]int{
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+		{1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+		{1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+		{1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+		{1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+		{1, 0, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	}
+	game := Game{
+		World:   worldMap,
+		Player:  Player{X: 2, Y: 2},
+		PowerOn: true,
+	}
 }
