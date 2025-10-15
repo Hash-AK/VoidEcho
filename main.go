8c7e467ddc217c1fca11050c1f7615a895e3bb35
Hash-AK <175879022+Hash-AK@users.noreply.github.com>
2025-10-15 18:07:09 -0400

    added a switch case to know when the game is in room mode or in grid mode (outside and inside with no power/inside with power)

--- main.go
+++ main.go
@@ -7,53 +7,55 @@ import (
 	"strings"
 )
 
+const (
+	ModeRoom = "room"
+	ModeGrid = "grid"
+)
+
 type Game struct {
 	World    [][]int
 	Player   Player
 	PowerOn  bool
 	KnownMap [][]bool
+	GameMode string
 }
 type Player struct {
-	X         int
-	Y         int
-	Inventory map[string]*Item
+	X           int
+	Y           int
+	Inventory   map[string]*Item
+	CurrentRoom *Room
 }
 type Item struct {
 	Name        string
 	Description string
 }
+type Exit struct {
+	Description string
+	Destination *Room
+}
+type Room struct {
+	Name        string
+	Description string
+	Exits       map[string]*Exit
+	Items       map[string]*Item
+}
 
 func main() {
-	// 0 mean empty space
-	// 1 means wall
-	// 2 means locked door
-	// 3 means stuf you gotta fidn to unlock the door
-	worldMap := [][]int{
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
-		{1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
-		{1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
-		{1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
-		{1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
-		{1, 0, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	}
-	game := Game{
-		World:   worldMap,
-		Player:  Player{X: 2, Y: 2},
-		PowerOn: true,
-	}
 	reader := bufio.NewReader(os.Stdin)
+	game := Game{
+		GameMode: ModeRoom,
+	}
 	for {
 		fmt.Println("")
 		fmt.Println(">")
 		input, _ := reader.ReadString('\n')
 		cleanInput := strings.TrimSpace(input)
 		fmt.Printf("You entered the command :%s\n", cleanInput)
+		switch game.GameMode {
+		case ModeRoom:
+			//stuff for when outside
+		case ModeGrid:
+			//stuff for when no light and when inside in general
+		}
 	}
 }
